89d5a650af0db9eb8cb4a59a43c0430d7507f0f5
ofdove <ofdove528@outlook.com>
2022-07-09 10:44:38 +0800

    fatTree added

--- fatTree.py
+++ fatTree.py
@@ -56,6 +56,3 @@ class MyTopo(Topo):
  
  
 topos = {"mytopo":(lambda:MyTopo())}
-————————————————
-版权声明：本文为CSDN博主「简vae」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/qq_49588762/article/details/115296838
